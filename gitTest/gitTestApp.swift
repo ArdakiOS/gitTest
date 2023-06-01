ba704ce30e89e38b0dd9d4ecaa5a491d11c21ee5
Ardak <luxr2ge@gmail.com>
2023-06-01 22:40:13 +0600

    Initial Commit

---
+++ gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by Ardak Tursunbayev on 01.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
